3ad6360d2b94bda2f948a8362537542ef8e4363d
Hector Hernandez <Hector.HernandezRosas@gmail.com>
2015-10-18 20:08:11 -0700

    validation throws in constructor --- bad

--- mocks/TimeService.cpp
+++ mocks/TimeService.cpp
@@ -1,10 +1,22 @@
 #include "TimeService.hpp"
+#include <stdexcept>
 
 using namespace Camax;
+using namespace std;
 
 TimeService::TimeService(int minute, int day)
 {
   //TODO: Validate ranges
+  const int hoursInDay = 24;
+  const int minutesInHour = 60;
+  if(minute < 0 || minute > ((hoursInDay * minutesInHour) - 1))
+      throw range_error("minutes must be between 0 and 1439");
+
+  const int daysInWeek = 7;
+  if(day < 1 || day > (daysInWeek))
+        throw range_error("days must be between 1 and 7");
+
+
   time.minuteOfDay = minute;
   time.dayOfWeek = day;
 }
